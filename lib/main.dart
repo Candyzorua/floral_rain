e61ed78adc6900616e3647167d4695db25a49db8
Candyzorua <jiaying.ong2@gmail.com>
2023-09-12 16:33:45 -0700

    [FIX] Cleared firebase references in Gradle files

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:floral_rain/helpers/route_generator.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 main() async {
   runApp(const ProviderScope(child: MyApp()));
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
 }
 
 class MyApp extends StatelessWidget {
